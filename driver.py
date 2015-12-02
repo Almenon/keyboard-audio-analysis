ea88dc9ffa0f3f5319b4d2122b4b3ddb4c10d9d2
Almenon <Almenon@users.noreply.github.com>
2015-12-01 17:40:28 -0800

    Create driver.py

---
+++ driver.py
@@ -0,0 +1,17 @@
+import soundfile as sf
+import matplotlib.pyplot as plt
+import audioextraction as extract
+# http://matplotlib.org/users/pyplot_tutorial.html
+import sklearn
+#import sklearn
+
+data, samplerate = sf.read('Recording_4_.wav')
+plt.plot(data)
+extract.
+plt.show()
+
+# extract features
+
+# sklearn.cluster.KMeans()
+# http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html#sklearn.cluster.KMeans
+# plot clusters
