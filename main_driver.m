5f0796a5e1b54922952f49d2e3c8a602804ace4d
Almenon <username@users.noreply.github.com>
2015-12-10 18:42:13 -0800

    fixed driver

--- main_driver.m
+++ main_driver.m
@@ -8,14 +8,16 @@ home = 'c:\dev\Matlab\Audio Analysis\';
 mirStruct = getdata(home,'Data_Set_Silence_Truncate_n35DB\5_key_Press_normal');
 data = mirgetdata(mirStruct); 
 mapped_data = containers.Map(letters,data);
-scatter(mapped_audio('A'),mapped_audio('B'))
 
 % segment it if neccesary
 % compress data if faster run-time needed
 
 % extract features
 
-    
-    
-% do machine learning
+features(1,:) = mirgetdata(mirrms(mirStruct));
+features(2,:) = mirgetdata(mirflatness(mirStruct));
 
+scatter(features(1,:),features(2,:))
+
+
+% do machine learning
