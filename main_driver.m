47c2e8b1310e24b02e266a7cf9fc3af040648122
Almenon <username@users.noreply.github.com>
2015-12-17 08:05:29 -0800

    Changed main_driver.m
    SVM now tests and trains on different data, as it should.  There is now
    also an analysis of the results.

--- main_driver.m
+++ main_driver.m
@@ -1,32 +1,52 @@
-% Trains a machine learning algorithim to 
-% be able to classify key-stroke sounds
+% Trains a SVM to be able to classify key-stroke sounds
 
 letters = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',...
     'P,','S','T','U','V','W','X','Y','Z'
 };
 home = 'c:\dev\Matlab\Audio Analysis\';
 
-% Trim silence at ends if desired
-% mirStruct = miraudio('folder','trim','trimthreshold',.03)
-% mirsave(mirStruct)
 
 % extract features (comment this out if features are already extracted)
 
 %features = getfeatures(home,['Keystrokes\Single_Key_Press_test']);
-%mapped_features = containers.Map(letters,features); % features by letter if desired
-
+load('training_features.mat');
+load('testing_features.mat');
+mapped_features = containers.Map(letters,features); 
+%features by letter if desired
 
 % visualize if desired
 % if using more than 3 features use PCA to compress
 % into lower-dimensional space, then visualize
-scatter(features(1,:),features(2,:))
-xlabel('feature 1'); ylabel('feature 2');
+% scatter(features(1,:),features(2,:))
+% xlabel('feature 1'); ylabel('feature 2');
 
 % do machine learning
+% Right now machine learning only works with 25 Keystroke dataset
 
-labels = zeros(1,26*5);
-labels(1:5) = 1; % categorize A
-SVMStruct = svmtrain(features',labels); %#ok<SVMTRAIN>
-% For now just test on same data
-results = svmclassify(SVMStruct,features'); %#ok<SVMCLASSIFY>
-error_rate = length(find(results==1)-5)/130;
+j = 1;
+results = [];
+num_samples = length(training_features);
+
+for i = 1:12:num_samples % 12 samples each letter
+    labels = zeros(1,num_samples);
+    labels(i:i+11) = 1; % categorize letter
+    SVMStruct = svmtrain(training_features',labels,...
+        'method','LS'); %#ok<SVMTRAIN>
+    % For now just test on same data
+    results = [results svmclassify(SVMStruct,testing_features')]; %#ok<SVMCLASSIFY>
+    correct_classifications = length(find(results(i:i+11,j)==1));
+    false_positive_rate(j) = length(find(results(:,j)==1)-correct_classifications)/118;
+    false_negative_rate(j) = (12-correct_classifications)/12;
+    j = j+1;
+end
+avg_fp = mean(false_positive_rate(1:26));
+avg_fn = mean(false_negative_rate(1:26));
+% Sensitivity is 1-false_negative_rate
+% Specificity is 1-false_positive_rate
+plot(false_positive_rate*100)
+xlabel('letters a-z')
+ylabel('false positive rate')
+title('False Positive Rate of SVM classifier for each letter')
+
+% to do: input results into classPerf to analyze performance more easily
+% http://www.mathworks.com/help/bioinfo/ref/classperf.html
